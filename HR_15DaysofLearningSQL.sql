2062fb21f2e66169373fc800411202c540b29c1a
rmnndhr <rumi.manandhar@gmail.com>
2023-07-11 01:44:14 -0500

    chore: Update program to display all 4 columns
    Previous program only provided the first, third, and forth columns,
    i.e. submission_date, hacker_id & name of those who had the most submissions that day.
    It was missing the second column, which display the total number of unique hacker(s),
    who submitted at least once each corresponding day.

--- HR_15DaysofLearningSQL.sql
+++ HR_15DaysofLearningSQL.sql
@@ -5,12 +5,67 @@ WITH t1 AS (
 	ON s.hacker_id = h.hacker_id
 	GROUP BY 1, 2, 3
 	ORDER BY 1, 2
-)
+),
+mar1 AS (
+	SELECT submission_date, hacker_id
+	FROM t1
+	WHERE submission_date = '2016-03-01'
+	GROUP BY 1, 2
+	ORDER BY 1
+),
+mar2 AS (
+	SELECT t1.submission_date, t1.hacker_id
+	FROM t1
+	INNER JOIN mar1
+	ON t1.hacker_id = mar1.hacker_id AND t1.submission_date = '2016-03-02'	
+),
+mar3 AS (
+	SELECT t1.submission_date, t1.hacker_id
+	FROM t1
+	INNER JOIN mar2
+	ON t1.hacker_id = mar2.hacker_id AND t1.submission_date = '2016-03-03'
+),
+mar4 AS (
+	SELECT t1.submission_date, t1.hacker_id
+	FROM t1
+	INNER JOIN mar3
+	ON t1.hacker_id = mar3.hacker_id AND t1.submission_date = '2016-03-04'
+),
+mar5 AS (
+	SELECT t1.submission_date, t1.hacker_id
+	FROM t1
+	INNER JOIN mar4
+	ON t1.hacker_id = mar4.hacker_id AND t1.submission_date = '2016-03-05'
+),
+mar6 AS (
+	SELECT t1.submission_date, t1.hacker_id
+	FROM t1
+	INNER JOIN mar5
+	ON t1.hacker_id = mar5.hacker_id AND t1.submission_date = '2016-03-06'
+),
 t2 AS (
+	SELECT * 
+	FROM mar1 
+	UNION SELECT * 
+	FROM mar2
+	UNION SELECT * 
+	FROM mar3
+	UNION SELECT * 
+	FROM mar4
+	UNION SELECT * 
+	FROM mar5
+	UNION SELECT * 
+	FROM mar6
+	ORDER BY 1
+),
+t3 AS (
 	SELECT submission_date, hacker_id, name, daily_subs,
 		NTILE(500) OVER (PARTITION BY submission_date ORDER BY daily_subs DESC, hacker_id) AS percentile
 	FROM t1
 )
-SELECT submission_date, hacker_id, name
-FROM t2
-WHERE percentile = 1;
+SELECT t3.submission_date, COUNT(*) AS daily_sub, t3.hacker_id, t3.name
+FROM t3
+INNER JOIN t2
+ON t3.submission_date = t2.submission_date
+WHERE percentile = 1
+GROUP BY 1, 3, 4;
